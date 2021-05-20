e0c2176cdd1bb1bebdbb83303afb78a7ad0aa6ab
Michael Power <michael@marshallpower.co.uk>
2021-05-20 16:06:53 +0100

    API added

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,12 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      message: ""
     };
+  }, 
+  async mounted() {
+    const { text } = await (await fetch("/api/message")).json();
+    this.message = text;
   }
 };
 </script>
